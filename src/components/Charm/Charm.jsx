3972f80046f7cb05a95e8dde3c2431ba7e474a38
Tryncha <juanes.aguirre26@gmail.com>
2025-09-05 15:37:10 -0500

    CharmTooltip created

--- src/components/Charm/Charm.jsx
+++ src/components/Charm/Charm.jsx
@@ -1,14 +1,34 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useLoadout } from '../../hooks/useLoadout';
 import './Charm.css';
 
+const CharmTooltip = ({ id, name, notchesCost, description, isHovering }) => {
+  if (!isHovering) return null;
+
+  return (
+    <div className="CharmTooltip">
+      <h3>{name}</h3>
+      <div className="CharmTooltip-notchesContainer">
+        {Array.from({ length: notchesCost }, (_, i) => (
+          <img
+            key={`notches-cost-${id}-${i + 1}`}
+            src="ui/notch-cost-on.png"
+            alt={`Notch #${i + 1}`}
+            width={30}
+            height={30}
+          />
+        ))}
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+};
+
 const Charm = ({ charmObj, isSelected }) => {
   const { setCharm } = useLoadout();
-  const { name, notchesCost, description, imgSrc } = charmObj;
+  const { id, name, notchesCost, description, imgSrc } = charmObj;
 
   const [isHovering, setIsHovering] = useState(false);
-
-  const charmRef = useRef(null);
 
   function handleClick() {
     setCharm(charmObj);
@@ -23,20 +43,29 @@ const Charm = ({ charmObj, isSelected }) => {
   }
 
   return (
-    <div
-      className={isSelected ? 'Charm Charm--isSelected' : 'Charm'}
-      onClick={handleClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <img
-        src={imgSrc}
-        alt={name}
-        width={80}
-        height={80}
-        title={name}
-        draggable={false}
+    <div className="CharmContainer">
+      <CharmTooltip
+        id={id}
+        name={name}
+        notchesCost={notchesCost}
+        description={description}
+        isHovering={isHovering}
       />
+      <div
+        className={isSelected ? 'Charm Charm--isSelected' : 'Charm'}
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img
+          src={imgSrc}
+          alt={name}
+          width={80}
+          height={80}
+          title={name}
+          draggable={false}
+        />
+      </div>
     </div>
   );
 };
