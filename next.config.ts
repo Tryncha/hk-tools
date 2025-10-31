12d2bf31cc3e778ce9db77c37ebd6c500ec0c5dd
Tryncha <juanes.aguirre26@gmail.com>
2025-10-31 16:53:51 -0500

    another test

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from 'next';
 
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  // basePath: isProd ? '/hollow-bench' : '',
-  // assetPrefix: isProd ? '/hollow-bench/' : '',
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: isProd ? '/hollow-bench' : '',
+  assetPrefix: isProd ? '/hollow-bench/' : ''
 };
 
 export default nextConfig;
